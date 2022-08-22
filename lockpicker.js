a4619bc17cea2c4e2c24e0a3ed228598bd5bcde2
Johannesgyr <62520496+Johannesgyr@users.noreply.github.com>
2022-08-22 22:13:45 +0200

    basic implementation of sweetspot

--- lockpicker.js
+++ lockpicker.js
@@ -3,6 +3,8 @@ let canvas = document.getElementById("can")
 let ctx = canvas.getContext("2d")
 ctx.font = "13px Arial"
 
+let pinSound = new Audio('assets/pin.mp3')
+
 
 
 canvas.addEventListener("click", (event) => {
@@ -23,23 +25,37 @@ function getRandomInt(min, max){
 }
 
 
+const marginOfError = 500;
+const borderSize = 2500;
+
+let sweetSpotFound = false;
 let currentX = 0;
 let currentY = 0;
-let sweetSpotX = getRandomInt(-2500, 2500);
-let sweetSpotY = getRandomInt(-2500, 2500);
+let sweetSpotX = getRandomInt(-borderSize, borderSize);
+let sweetSpotY = getRandomInt(-borderSize, borderSize);
 canvas.addEventListener("mousemove", (event) =>{
     if(canvasActive){
         if(event.movementX < 0){
-            currentX = Math.max(-2500, currentX + event.movementX)
+            currentX = Math.max(-borderSize, currentX + event.movementX)
         }else if(event.movementX > 0){
-            currentX = Math.min(2500, currentX + event.movementX)
+            currentX = Math.min(borderSize, currentX + event.movementX)
         }
         if(event.movementY < 0 ){
-            currentY = Math.max(-2500, currentY + event.movementY);
+            currentY = Math.max(-borderSize, currentY + event.movementY);
         }else if(event.movementY > 0){
-            currentY = Math.min(2500, currentY + event.movementY)
+            currentY = Math.min(borderSize, currentY + event.movementY)
         }
         ctx.clearRect(0, 0, canvas.clientWidth, canvas.height)
+        if(currentX - sweetSpotX < marginOfError && currentX - sweetSpotX > -marginOfError && currentY - sweetSpotY < marginOfError && currentY - sweetSpotY > -marginOfError){
+            sweetSpotFound = true;
+            console.log("bingo")
+            ctx.fillStyle = "red";
+
+        }else{
+            sweetSpotFound = false;
+            ctx.fillStyle = "black";
+        }
+        ctx.fill();
         ctx.fillText("Current X: " + currentX, 10, 20)
         ctx.fillText("Current Y: " + currentY, 10, 40)
         ctx.fillText("Target X: " + sweetSpotX, 10, 80)
@@ -48,8 +64,8 @@ canvas.addEventListener("mousemove", (event) =>{
 })
 
 document.addEventListener("keydown", (event)=>{
-    if(event.key == 'f' || event.key == 'F'){
-        
+    if(sweetSpotFound && (event.key == 'f' || event.key == 'F')){
+        pinSound.play();
     }
 })
 
